e90618ec3de6ff5cc53b0264db7080eab2cec936
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-10-18 09:29:04 +0200

    exercise with ruby arrays

--- my_group.rb
+++ my_group.rb
@@ -1,4 +1,10 @@
 my_group =[];
-person_1 = {name: "Elvita", gender: "f", age: 29};
-person_2 = {name: "Anna", gender: "f", age: 61};
-person_3 = {name: "Carl", gender: "m", age: 17};
+person_1 = {name: "Elvita", gender: "woman", age: 29};
+person_2 = {name: "Anna", gender: "woman", age: 61};
+person_3 = {name: "Carl", gender: "man", age: 17};
+
+my_group =[person_1, person_2, person_3];
+
+my_group.each do |person|
+puts "#{person [:name]} is a #{person [:age]} years old #{person [:gender]}."
+end
