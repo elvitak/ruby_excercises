8145c87670f0535142050e397e8c7cc5db87bcb2
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-10-18 13:13:19 +0200

    practice each loop

--- my_group.rb
+++ my_group.rb
@@ -8,3 +8,17 @@ my_group =[person_1, person_2, person_3];
 my_group.each do |person|
 puts "#{person [:name]} is a #{person [:age]} years old #{person [:gender]}."
 end
+
+
+fimls = []
+film_1 = {title: "ABC", year: 2002}
+film_2 = {title: "HarryPotter", year: 1999}
+film_3 = {title: "Innovation", year: 2021}
+film_4 = {title: "I dont know", year: 2015}
+film_5 = {title: "isn't this already enough", year: 2013}
+
+films = [film_1, film_2, film_3, film_4, film_5]
+
+films.each do |film|
+ puts "#{film[:year]}"
+ end
