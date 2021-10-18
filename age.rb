6ca237c8872d1cf4e4f973134f299ef1029eea7b
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-10-18 14:47:39 +0200

    practice get.chomp.to_i for numbers

---
+++ age.rb
@@ -0,0 +1,9 @@
+puts "How old are you now?"
+currentAge = gets.chomp.to_i
+
+puts "In 10 years you will be:"
+puts currentAge + 10
+puts "In 20 years you will be:"
+puts currentAge + 20
+puts "In 30 years you will be:"
+puts currentAge + 30
